fd793d93a968b15281177a1fa95c076775905380
Michał Kucharski <michal.kucharski@creobit.pl>
2021-05-26 15:04:15 +0200

    Add conditional component loading different for mobile and desktop. Add transition while changing components with router.

--- src/App.vue
+++ src/App.vue
@@ -1,47 +1,51 @@
 <template>
   <div id="app" class="flex flex-wrap">
     <CV></CV>
-    <div class="flex flex-col w-4/5">
+    <div v-if="!isMobile()" class="flex flex-col w-4/5">
       <nav class="flex justify-evenly w-full h-16 bg-black text-green-500 uppercase">
-        <router-link to="/" class="self-center">Start</router-link>
-        <router-link to="/About" class="self-center">O mnie</router-link>
-        <router-link to="/Skills" class="self-center">Umiejętności</router-link>
-        <router-link to="/Porfolio" class="self-center">Moje projekty</router-link>
-        <router-link to="/Contact" class="self-center">Kontakt</router-link>
+        <router-link to="/" class="self-center hover:text-green-200 transition duration-200">Start</router-link>
+        <router-link to="/About" class="self-center hover:text-green-200 transition duration-200">O mnie</router-link>
+        <router-link to="/Skills" class="self-center hover:text-green-200 transition duration-200">Umiejętności</router-link>
+        <router-link to="/Porfolio" class="self-center hover:text-green-200 transition duration-200">Moje projekty</router-link>
+        <router-link to="/Contact" class="self-center hover:text-green-200 transition duration-200">Kontakt</router-link>
       </nav>
-      <router-view class="flex w-full"></router-view>
+      <transition name="fade" mode="out-in">
+        <router-view class="flex w-full"></router-view>
+      </transition>      
     </div>
+    <div v-else>
+      <About></About>
+    </div>    
   </div>
 </template>
 
 <script>
   import CV from '@/views/CV.vue'
+  import About from '@/views/About.vue'
   export default {
     components: {
       CV,
+      About,
+    },
+    methods: {
+      isMobile() {
+        if(/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)) {
+          return true
+        } else {
+          return false
+        }
+      }
     }
   }
 </script>  
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+.fade-enter-active, .fade-leave-active {
+  transition-duration: 0.5s;
+  transition-property: opacity;
+  transition-timing-function: ease;
 }
-
-#nav {
-  padding: 30px;
-}
-
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-#nav a.router-link-exact-active {
-  color: #42b983;
+.fade-enter, .fade-leave-active {
+  opacity: 0
 }
 </style>
