12b8921a38412dc03e086d63e9dcf303d7df4759
Michał Kucharski <michal.kucharski@creobit.pl>
2021-06-13 18:03:33 +0200

    Add working modal in skills component

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,43 +1,45 @@
 <template>
-  <div class="home flex flex-col items-center justify-center">
-    <div class="bg-gray-100 border-2 border-green-500 rounded-lg">
-        <blockquote class="flex flex-wrap flex-col bg-white text-green-700 border-l-8 italic border-green-500 px-4 py-3">
-          {{quoteText}}
-          <span class="flex justify-end text-sm text-green-700 font-semibold pt-2 underline ">{{quoteAuthor}}</span>
-        </blockquote>
+    <div class="home flex flex-col items-center justify-center">
+        <div class="bg-gray-100 border-2 border-green-500 rounded-lg">
+            <blockquote class="flex flex-wrap flex-col bg-white text-green-700 border-l-8 italic border-green-500 px-4 py-3">
+                {{ quoteText }}
+                <span class="flex justify-end text-sm text-green-700 font-semibold pt-2 underline ">{{ quoteAuthor }}</span>
+            </blockquote>
+        </div>
+        <h2>Witam na mojej stronie. Zachęcam do zapoznania się z moim portfolio.</h2>
     </div>
-    <h2>Witam na mojej stronie. Zachęcam do zapoznania się z moim portfolio.</h2>
-  </div>
 </template>
 
 <script lang="ts">
-
 export default {
-  name: 'Home',
-    data: function () {
-    return {
-      quoteText: '' as string,
-      quoteAuthor: '' as string,
-    }
-  },
-  methods: {
-    quoteBody() {
-        fetch("https://type.fit/api/quotes").then((response) => {
-        return response.json();
-      }).then((data) => {
-        const quotes = data;
-        function getRandomQuote() : number {
-        return  Math.floor(Math.random() * (quotes.length - 1))
-        }    
-        this.quoteText = quotes[getRandomQuote()].text;
-        this.quoteAuthor = quotes[getRandomQuote()].author;    
-      }).catch(function(err){
-        console.log(err);
-      })
+    name: "Home",
+    data: function() {
+        return {
+            quoteText: "" as string,
+            quoteAuthor: "" as string,
+        };
     },
-  },
-  created () {
-    this.quoteBody();
-  } 
-}
+    methods: {
+        quoteBody() {
+            fetch("https://type.fit/api/quotes")
+                .then((response) => {
+                    return response.json();
+                })
+                .then((data) => {
+                    const quotes = data;
+                    function getRandomQuote(): number {
+                        return Math.floor(Math.random() * (quotes.length - 1));
+                    }
+                    this.quoteText = quotes[getRandomQuote()].text;
+                    this.quoteAuthor = quotes[getRandomQuote()].author;
+                })
+                .catch(function(err) {
+                    console.log(err);
+                });
+        },
+    },
+    created() {
+        this.quoteBody();
+    },
+};
 </script>
--- src/views/Skills.vue
+++ src/views/Skills.vue
@@ -1,5 +1,5 @@
 <template>
-    <div id="skills" class="flex flex-col justify-evenly text-center">
+    <div id="skills" class="relative flex flex-col justify-evenly text-center">
         <div>
             <h2 class="uppercase text-xl">Umiejętności</h2>
             <div class="flex justify-evenly">
@@ -19,10 +19,16 @@
             </div>
             <h2 class="uppercase text-xl">Do nauki</h2>
             <div class="flex justify-evenly">
-                <img src="../assets/nodejs.svg" alt="Node" width="100" height="100" class="cursor-pointer" id="node" @click="showModal" />
-                <img src="../assets/mongodb.svg" alt="MongoDB" width="100" height="100" class="cursor-pointer" id="mongodb" @click="showModal" />
-                <img src="../assets/typescript.svg" alt="TypeScript" width="100" height="100" class="cursor-pointer" id="typescript" @click="showModal" />
-                <img src="../assets/jest.svg" alt="Jest" width="100" height="100" class="cursor-pointer" id="jest" @click="showModal" />
+                <img src="../assets/nodejs.svg" alt="Node" width="100" height="100" class="cursor-pointer" id="nodejs" />
+                <img src="../assets/mongodb.svg" alt="MongoDB" width="100" height="100" class="cursor-pointer" id="mongodb" />
+                <img src="../assets/typescript.svg" alt="TypeScript" width="100" height="100" class="cursor-pointer" id="typescript" />
+                <img src="../assets/jest.svg" alt="Jest" width="100" height="100" class="cursor-pointer" id="jest" />
+            </div>
+            <div class="hidden bg-gray-900 bg-opacity-90 absolute inset-0 w-full h-full" id="modal" @click="hideModal"></div>
+            <div class="hidden absolute w-2/4 h-2/3 border-solid border-2 rounded-md border-green-600 top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 bg-white flex flex-col items-center justify-center" id="modalContent">
+                <img src="" alt="" id="modalImg" width="100" height="100" />
+                <h2>{{ this.technologyName }}</h2>
+                <span class="absolute top-4 right-6 text-4xl cursor-pointer" @click="hideModal">X</span>
             </div>
         </div>
     </div>
@@ -33,13 +39,42 @@ export default {
     name: "Skills",
     data() {
         return {
-            html: [1, 2, 3, 4],
+            technologyName: "",
+            html: ["tworzenie semantycznego kodu HTML", "znajomość atrybutów", "tworzenie list i formularzy", "praca z obrazami i video"],
+            css: ["znajomość tailwind.css i bootstrapa", "znajomość flexboxa i grida", "tworzenie animacji"],
+            sass: ["umiejętność konfiguracji", "znajomość składni scss", "znajomość mixins, functions i variables"],
+            js: ["znajomość standardu ES6", "asynchroniczność", "korzystanie z API", "umiejętność korzystania z zewnętrznych rozwiązań", "programowanie zorientowane obiektowo", "praca z tablicami"],
+            vue: [],
+            laravel: [],
         };
     },
     methods: {
         showModal(e) {
+            const modal = document.querySelector("#modal");
+            const modalImg = document.querySelector("#modalImg");
+            const modalContent = document.querySelector("#modalContent");
+
+            const eraseParagraphs = modalContent.querySelectorAll("p");
+            eraseParagraphs.forEach((item) => item.remove());
+
+            modalImg.setAttribute("src", e.target.currentSrc);
+            modalImg.setAttribute("alt", e.target.id);
+
+            this.technologyName = e.target.id.toUpperCase();
             const data = this[e.target.id];
-            data.forEach((item) => console.log(item));
+
+            data.forEach((item) => {
+                const paragraph = document.createElement("p");
+                paragraph.innerText = item;
+                modalContent.appendChild(paragraph);
+            });
+
+            modal.classList.remove("hidden");
+            modalContent.classList.remove("hidden");
+        },
+        hideModal() {
+            modal.classList.add("hidden");
+            modalContent.classList.add("hidden");
         },
     },
 };
